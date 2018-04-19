39a76db75b04fc1916ed53773151069b4f61f90b
BananaRey <37335483+BananaRey@users.noreply.github.com>
2018-04-19 09:06:59 -0500

    Add files via upload

--- adveccion.cpp
+++ adveccion.cpp
@@ -14,14 +14,14 @@ double gauss(double x,double y, double sigma);
 int main(){
  double C=1;
  double T=0.5;
- double Delta_x=0.1;
- double Delta_t=0.1;
+ double Delta_x=0.01;
+ double Delta_t=0.01;
 
 
 
  double t=0;
  double x=1;
- double h=0.1;
+ double h=0.001;
  double N=3/h;
  double intervalo = 2.0;
  int n_x= (intervalo/Delta_x)+1;
@@ -36,18 +36,18 @@ int main(){
 
  
  for(int i=0;i<n_x;i++){
- u_old[i]=x_en_t0(x,0.0);  
- x+=Delta_x;
+ u_old[i]=gauss(i*Delta_x,0.0,0.1);
+ 
  }
  for(int j=1;j<n_t;j++){
-  for(int i=1;i<n_x;i++){
-   u_new[i]=u_old[i]-((gauss(x,0,0.1)*Delta_t)/Delta_x)*(u_old[i]-u_old[i-1]);	
+  for(int i=1;i<n_x-1;i++){
+   u_new[i]=u_old[i]-((gauss(i*Delta_x,0,0.1)*Delta_t)/Delta_x)*(u_old[i]-u_old[i-1]);	
    x+=Delta_x;	
    }
   }
  for(int i=0;i<n_x;i++){ 
- cout<<u_new[i]<<endl;
- x+=Delta_x;	
+ cout<<i*Delta_x<<" "<<u_new[i]<<endl;
+	
  }
  
 return(0);
@@ -63,7 +63,7 @@ double x_en_t0(double x,double y){
 }
 double gauss(double x,double y, double sigma){
 
-return pow(1/(2*3.1415*sigma*sigma),(0.5))*pow(2.71828,(-(x*x)/2*(sigma*sigma)));
+return (pow(1/(2*3.1415*sigma*sigma),(0.5))*pow(2.71828,(-((x-1)*(x-1))/2*(sigma*sigma))));
 }
 
 
